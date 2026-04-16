40b3748b61ed539517d1e4ba43956ca31ec9e079
lebaodiep0809 <o962885401@gmail.com>
2026-04-16 14:19:07 +0700

    bo sung tsc

---
+++ src/main/java/org/example/TCS_9_UnHappy.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class TCS_9_UnHappy {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://saucelabs.com/request-demo");
+
+        WebElement busEmail = driver.findElement(By.xpath("//input[@id='Email']"));
+        busEmail.sendKeys("nguyenttlink@gmail.com");
+
+        WebElement fistName = driver.findElement(By.xpath("//input[@id='FirstName']"));
+        fistName.sendKeys("Hoa");
+
+        WebElement lastName = driver.findElement(By.xpath("//input[@id='LastName']"));
+        lastName.sendKeys("Nguyeexn");
+
+        WebElement company = driver.findElement(By.xpath("//input[@id='Company']"));
+        company.sendKeys("Abc");
+
+        WebElement phoneNub = driver.findElement(By.xpath("//input[@id='Phone']"));
+        phoneNub.sendKeys("095478147");
+
+        driver.findElement(By.xpath("//select[@id ='Country']")).click();
+        driver.findElement(By.xpath("//select[@id ='Country']/descendant::option [@value ='United States']")).click();
+
+        driver.findElement(By.xpath("//select[@id ='State']")).click();
+        driver.findElement(By.xpath("//select[@id ='State']/descendant::option [@value ='Alaska']")).click();
+
+        driver.findElement(By.xpath("//select[@id ='Solution_Interest__c']")).click();
+        driver.findElement(By.xpath("//select[@id ='Solution_Interest__c']/descendant::option [@value ='Sauce AI Agents']")).click();
+
+        WebElement comment = driver.findElement(By.id("Sales_Contact_Comments__c"));
+        comment.sendKeys("abc");
+
+        //Không click vào checkbox
+
+        WebElement btn = driver.findElement(By.className("mktoButton"));
+        btn.click();
+    }
+}
